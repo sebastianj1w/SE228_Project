557514856d65eefced2c157dc0cc844a76aec57d
sebastianj1w <sebastianj1w@outlook.com>
2019-04-21 15:46:17 +0800

    updated sqls

--- DDL&insert/ddl.sql
+++ DDL&insert/ddl.sql
@@ -1,15 +1,22 @@
 create database ebook collate Chinese_PRC_CI_AS
 go
 
-create table bookBasic
+create table books
 (
 	title varchar(50) not null,
 	author varchar(50) not null,
 	publisher varchar(50) not null,
 	price numeric(8,2) not null,
-	ID varchar(15) not null,
+	ID varchar(50) not null
+		constraint bookBasic_pk
+			primary key nonclustered,
 	stock int,
-	ISBN nchar(13) not null
+	ISBN nchar(13) not null,
+	rate float,
+	words float,
+	publishDate varchar(11),
+	abstraction text,
+	catalogue text
 )
 go
 
@@ -21,53 +28,46 @@ create unique index t_booklist_ISBN_uindex
 	on books (ISBN)
 go
 
-create table bookDetail
+create table orders
 (
-	ID varchar(15) not null
-		constraint t_bookdetail_t_booklist_ID_fk
-			references books (ID),
-	rate float,
-	publishDate varchar(11),
-	words float,
-	abstraction text,
-	catalogue text
+	total money not null,
+	userID varchar(50),
+	orderID varchar(50) not null
+		constraint orders_pk
+			primary key nonclustered,
+	state int,
+	title varchar(500),
+	date numeric(30)
 )
 go
 
 create table items
 (
-	orderID int not null
-		constraint t_items_t_order_orderID_fk
-			references t_order (orderID),
-	bookID varchar(15) not null
-		constraint t_items_t_booklist_ID_fk
-			references booklist (ID),
+	orderID varchar(50) not null
+		constraint items_orders_orderID_fk
+			references orders,
+	bookID varchar(50) not null
+		constraint items_books_ID_fk
+			references books,
 	amount int not null,
 	value money not null
 )
 go
 
-create table orders
-(
-	total money not null,
-	userID varchar(50)
-		constraint t_order_t_userinfo_id_fk
-			references [user] (id),
-	orderID int identity
-)
-go
-
-create unique index t_order_orderID_uindex
+create unique index orders_orderID_uindex
 	on orders (orderID)
 go
 
 create table users
 (
-	id varchar(50) not null,
+	id varchar(50) not null
+		constraint users_pk
+			primary key nonclustered,
 	iden varchar(10) not null,
 	password varchar(50) not null,
 	ban bit not null,
-	mail varchar(50)
+	mail varchar(50),
+	cart varchar(2000)
 )
 go
 
